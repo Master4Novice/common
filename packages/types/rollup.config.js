049ebce7a5b92af5bd8e77db8a3445c00aa7a145
Abhinav Dwivedi <a.dwivedispu@gmail.com>
2023-12-12 23:28:41 +0530

    Updated Rollup

--- packages/types/rollup.config.js
+++ packages/types/rollup.config.js
@@ -1,23 +1,29 @@
 import typescript from '@rollup/plugin-typescript';
-import dts from "rollup-plugin-dts";
+import dts from 'rollup-plugin-dts';
+import copy from 'rollup-plugin-copy'
 
 const config = [
   {
     input: 'src/index.ts',
     output: [
         {
-           file: 'dist/index.cjs',
+           file: 'dist/cjs/index.cjs',
            format: 'cjs',
            sourcemap: true,
         },
         {
-            file: 'dist/index.mjs',
+            file: 'dist/es/index.mjs',
             format: 'es',
             sourcemap: true,
          }
     ],
     plugins: [
-        typescript()
+        typescript(),
+        copy({
+          targets: [
+            { src: ["package.json", "README.md", "../../LICENSE"], dest: "dist" }
+          ]
+        })
     ]
   }, {
     input: 'src/index.ts',
