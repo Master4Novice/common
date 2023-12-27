dd07bb991132b30a2cc255341652fe10de655fdf
Abhinav Dwivedi <a.dwivedispu@gmail.com>
2023-12-28 01:11:10 +0530

    Latest HttpStatus Changes

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -2,4 +2,3 @@ export { Gender } from './models/gender';
 export { TreeNode } from './models/TreeNode';
 export { Tree } from './models/Tree';
 export { Address } from './models/Address';
-export { setNameValue } from './utils/string-number';
--- packages/types/src/utils/string-number.ts
+++
@@ -1,7 +0,0 @@
-export function setNameValue (name: string, value: number) {
-    const NameValue = Object.freeze({name,value});
-    return {
-        value: () => { return NameValue.value},
-        toString: () => { return NameValue.name}
-    };
-}
